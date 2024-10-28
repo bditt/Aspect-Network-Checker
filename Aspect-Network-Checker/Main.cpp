ab089d8993f18f356550745358583820a8bbfc7f
Brian <ProjectElitist@gmail.com>
2024-10-28 04:28:20 -0400

    Added some basic proxy support.

--- Aspect-Network-Checker/Main.cpp
+++ Aspect-Network-Checker/Main.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <string>
 #include <random>
+#include <chrono>
 
 //Our includes.
 #include "thread_pool.hpp"
@@ -12,6 +13,10 @@ BS::thread_pool pool(max_thread_count); // Adjust the pool size as needed
 
 std::string const host = "https://nodec.mediathektv.com";
 int version = 11;
+
+std::string const proxy_ip = "http://udc.resi.gg:9999";
+std::string const proxy_username = "resi-ProjectElitist";
+std::string const proxy_password = "LC2WNWr6gWOXEnbJRSEK";
 
 const std::vector<std::string> proxies = {};
 
@@ -25,12 +30,27 @@ void curl_test()
 	if (curl)
 	{
 		curl_easy_setopt(curl, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_2_0);
+		curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
+		curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
 		curl_easy_setopt(curl, CURLOPT_NOBODY, 1L);
-		curl_easy_setopt(curl, CURLOPT_URL, host);
+		curl_easy_setopt(curl, CURLOPT_PROXY, proxy_ip.c_str());
+		curl_easy_setopt(curl, CURLOPT_PROXYUSERNAME, proxy_username.c_str());
+		curl_easy_setopt(curl, CURLOPT_PROXYPASSWORD, proxy_password.c_str());
+		curl_easy_setopt(curl, CURLOPT_URL, host.c_str());
 
 		while (true)
 		{
-			CURLcode res = curl_easy_perform(curl);
+			try
+			{
+				CURLcode res = curl_easy_perform(curl);
+				if (res != CURLE_OK)
+					std::cout << res << std::endl;
+				
+			}
+			catch (...)
+			{
+
+			}
 		}
 
 		curl_easy_cleanup(curl);
@@ -40,20 +60,39 @@ void curl_test()
 int main()
 {
 	srand((unsigned)time(NULL));
+
+	int time_limit = 15;
+
 	for (int i = 0; i < max_thread_count; i++)
 	{
 		pool.submit_task([] { curl_test(); });
 	}
 
+	auto beg = std::chrono::high_resolution_clock::now();
+	int last_duration = 0;
 	while (true)
 	{
+		auto end = std::chrono::high_resolution_clock::now();
+		auto duration = std::chrono::duration_cast<std::chrono::seconds>(end - beg).count();
+		if (duration != last_duration)
+		{
+			std::cout << "Time Left: " << time_limit - duration << std::endl;
+			last_duration = duration;
+		}
+
+		if (duration > time_limit)
+			break;
+
 		auto running = pool.get_tasks_running();
 		auto refil = max_thread_count - running;
+		//std::cout << "Refil: " << refil << std::endl;
 		for (int i = 0; i < refil; i++)
 		{
 			pool.submit_task([] { curl_test(); });
 		}
 	}
+
+	std::cout << "Done!" << std::endl;
 
 	return EXIT_SUCCESS;
 }
